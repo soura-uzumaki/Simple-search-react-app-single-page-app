768576baaf6bf0dc838a0562c84962f16d0fa0a6
soura-uzumaki <soura0005@gmail.com>
2020-06-10 18:31:09 +0530

    Updates

---
+++ precache-manifest.379fd718db24fcbac3f490ce6b17d633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810a99f4477fa457c8818733a79bb391",
+    "url": "/Simple-search-react-app-single-page-app/index.html"
+  },
+  {
+    "revision": "6c04a1c796e4683006f5",
+    "url": "/Simple-search-react-app-single-page-app/static/css/main.b0f2d29a.chunk.css"
+  },
+  {
+    "revision": "87e7221daf34f4c82e20",
+    "url": "/Simple-search-react-app-single-page-app/static/js/2.10c52673.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Simple-search-react-app-single-page-app/static/js/2.10c52673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c04a1c796e4683006f5",
+    "url": "/Simple-search-react-app-single-page-app/static/js/main.3b486b92.chunk.js"
+  },
+  {
+    "revision": "660c253c1557b1cb7e00",
+    "url": "/Simple-search-react-app-single-page-app/static/js/runtime-main.608140e2.js"
+  }
+]);
